75de56976d22968be65d362100284f1d34e5390f
Gabogg07 <gabrieleduardogg@gmail.com>
2017-10-18 17:25:23 +0200

    City added

--- RiskInfoProject/src/be/sme/domain/City.java
+++ RiskInfoProject/src/be/sme/domain/City.java
@@ -2,4 +2,12 @@ package be.sme.domain;
 
 public class City {
     private String name;
+    private String state;
+    private String country;
+
+    private City (String name, String state, String country){
+        this.name=name;
+        this.state=state;
+        this.country=country;
+    }
 }
